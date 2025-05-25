99fc71b7c9b678ec6eacf306d1b21e0756836d86
Vishal7326 <vishalp65276@gmail.com>
2025-05-25 14:24:43 +0530

    Added Models and Tasks class and properties in the class , Also Made changes in the appsettings

---
+++ Models/Tasks.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagementApplication.Models
+{
+    public class Tasks
+    {
+        public Guid Id { get; set; } // Generates unique identifier for each task
+
+        public required string Title { get; set; } // Title of the task 
+
+        public string? Description { get; set; } // Description of the task  
+
+        public DateTime DueDate { get; set; } // Due date for the task
+
+        public bool IsCompleted { get; set; } // Indicates if the task is completed 
+    }
+}
